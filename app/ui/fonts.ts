47d954a4fe2c4beb698f687f0d9991b12cd17626
TomBoutin <144112026+TomBoutin@users.noreply.github.com>
2024-09-20 16:34:20 +0200

    Add files via upload

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'], display: 'swap' });
+const lusitana = Lusitana({ weight: '400', subsets: ['latin'], display: 'swap' });
+
+export { inter, lusitana };
